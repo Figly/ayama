cb6bb86ede54cee51ab02c7c9f0504221954c90e
Q <coetzee.vs@gmail.com>
2020-06-15 22:14:51 +0200

    fixing settings pt 1

--- src/ayama/settings.py
+++ src/ayama/settings.py
@@ -2,7 +2,6 @@ import logging.config as config
 
 from .settings_base import *  # noqa
 
-# TODO: should catch if dev or staging
 if not os.environ.get("ENVIRONMENT", False) or os.environ.get("ENVIRONMENT") in [
     "dev",
     "staging",
@@ -44,13 +43,14 @@ else:
     TEMPLATES[0].update({"APP_DIRS": False})
 
     # Setup static file serving
-    STATICFILES_FINDERS = [
-        "django.contrib.staticfiles.finders.FileSystemFinder",
-        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    ]
-
     # Configure static and media file access on GCS
     DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+
+    STATICFILES_FINDERS = [
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+        "django.contrib.staticfiles.finders.DefaultStorageFinder",
+        "django.contrib.staticfiles.finders.FileSystemFinder",
+    ]
 
     if os.environ.get("ENVIRONMENT") != "production":
         GS_BUCKET_NAME = "ayama-staging-assets"
@@ -58,9 +58,7 @@ else:
         GS_BUCKET_NAME = "ayama-production-production"
 
     STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-
     MEDIA_URL = "https://storage.googleapis.com/ayama-staging-assets/"
-
     STATIC_URL = "https://storage.googleapis.com/ayama-staging-assets/"
 
 PASSWORD_HASHERS = [
