b1d18e0fcd593b62afc755b2e3a5dcacaf209c62
Henk van Dyk <61801312+HenkvDyk@users.noreply.github.com>
2020-06-19 09:07:59 +0930

    tracked file added

---
+++ src/clients/models/__init__.py
@@ -0,0 +1,6 @@
+from .client_communication import ClientCommunication
+from .client_contact_detail import ClientContactDetail
+from .client_detail import ClientDetail
+from .client_employment_detail import EmploymentDetail
+from .dependent import Dependent
+from .rates_and_return import RatesAndReturn
