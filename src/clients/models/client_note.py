48a47d71b340271bccce69dbf0c4503effbe04aa
Henk van Dyk <61801312+HenkvDyk@users.noreply.github.com>
2020-06-24 13:32:56 +0930

    client-notes-html

--- src/clients/models/client_note.py
+++ src/clients/models/client_note.py
@@ -6,7 +6,6 @@ from .base import BaseModel
 ch_note_type = (
     ("note", "Note"),
     ("follow_up", "Follow Up"),
-    ("do_not_mention", "Do Not Mention"),
     ("preferences", "Preferences"),
     ("archive", "Archive"),
 )
@@ -18,15 +17,13 @@ class ClientNote(BaseModel):
     """
 
     client_id_fk = models.ForeignKey(
-        "ClientDetail", on_delete=models.CASCADE, related_name="notes"
+        "ClientDetail", on_delete=models.CASCADE, related_name="notes",
     )
 
-    title = models.CharField(
-        "Note Title", max_length=100, blank=True, null=True, default=str(datetime.now())
-    )
-    body = models.CharField("Note", max_length=2000)
+    title = models.CharField("Note Title", max_length=100,)
+    body = models.CharField("Note", max_length=5000,)
     note_type = models.CharField(
-        "Note Type", max_length=50, choices=ch_note_type, default="Note"
+        "Note Type", max_length=50, choices=ch_note_type, default="Note",
     )
 
     def __str__(self):
