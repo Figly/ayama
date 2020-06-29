ef26687bd2941509dbc61cc0defb57eb2c191e45
Q <coetzee.vs@gmail.com>
2020-06-29 08:44:06 +0200

    minor changes

--- configs/files/etc/gunicorn/gunicorn.py
+++ configs/files/etc/gunicorn/gunicorn.py
@@ -1,8 +1,11 @@
 import os
 
 bind = "0.0.0.0:8080"
-chdir = "/usr/src/app"
-loglevel = "DEBUG"
+
+if os.environ.get("ENVIRONMENT", None) is not None:
+    chdir = "/usr/src/app"
+
+loglevel = "WARNING"
 max_requests = 10000
 max_requests_jitter = 1000
 worker_class = "gevent"
@@ -11,5 +14,6 @@ workers = "3"
 errorlog = "-"
 accesslog = "-"
 
-if os.environ.get("DEBUG", "False").lower() == "true":
+if os.environ.get("DEBUG", False):
     reload = True
+    loglevel = "DEBUG"
