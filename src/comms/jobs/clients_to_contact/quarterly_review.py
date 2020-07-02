5155c46299d41cf1784f16db2756c05ec3cf503f
kyle <kylebrandin@gmail.com>
2020-07-02 22:12:03 +0200

    Ayama-3 base notifications module

--- src/comms/jobs/clients_to_contact/quarterly_review.py
+++ src/comms/jobs/clients_to_contact/quarterly_review.py
@@ -8,8 +8,9 @@ import django
 from django.core.mail import send_mail, send_mass_mail
 from django.db.models import Q
 
-from clients.models import client_communication, client_detail
-from practises.models import advisor_detail
+from clients.models.client_communication import ClientCommunication
+from practises.models.advisor_contact_detail import AdvisorContactDetail
+from practises.models.advisor_detail import AdvisorDetail
 
 log = logging.getLogger(__name__)
 
@@ -20,33 +21,56 @@ def run(**kwargs):
         today = datetime.today()
         three_months_ago = today - timedelta(days=3)
 
-        _subject = kwargs.get("subject", "No Subject")
-        _body = kwargs.get("body", "No body provided.")
-        _from = kwargs.get("from", "from@example.com")
-        _to = kwargs.get("to", "to@example.com")
-        _fail_silently = kwargs.get("fail_silently", False)
+        _subject = kwargs["subject"] or "Communication Reminder"
+        _body = kwargs["body"] or "Communication reminder job run."
+        _from = kwargs["from"] or "karelverhoeven@gmail.com"
+        _to = (
+            kwargs["to"]
+            or "kylebrandin@gmail.com,karelverhoeven@gmail.com,coetzee.vs@gmail.com"
+        )
+        _fail_silently = kwargs["fail_silently"] or False
 
         if not isinstance(_to, list):
             log.info("Destination address is not a list. Coercing to list.")
             _to = list(_to.split(","))
-        
-        datatuple = (
-            ('Communication Reminder', 'Communication reminder job run.', 'karelverhoeven@gmail.com', ['kylebrandin@gmail.com, karelverhoeven@gmail.com, coetzee.vs@gmail.com']),
+
+        clients_communication_history = ClientCommunication.objects.filter(
+            Q(last_date_email__lte=three_months_ago)
+            | Q(last_date_sms__lte=three_months_ago)
+            | Q(last_date_call__lte=three_months_ago)
+            | Q(last_date_face_to_face__lte=three_months_ago)
         )
 
-        clients_late_communication = client_communication.objects.filter(Q(last_date_email__lte = three_months_ago) | 
-                                                            Q(last_date_sms = three_months_ago) |
-                                                            Q(last_date_call = three_months_ago) |
-                                                            Q(last_date_face_to_face = three_months_ago))
-        
-        advisors_to_mail = clients_late_communication.filter(client_detail__advisor_id_fk).distinct("id") #noqa
+        advisors = AdvisorContactDetail.objects.all()
+        massMail = []
+        body = ""
+        to = []
 
-        message = ""
-        for advisor in advisors_to_mail:
-            message += clients_late_communication.filter(client_detail__advisor_id_fk = advisor.id).only("names", "surnames", "client_contact_fk__email_address")
-            datatuple.insert(('Communication Reminder', message, 'karelverhoeven@gmail.com', message.email_address))
+        for advisor in advisors:
+            advisor_clients_history = clients_communication_history.filter(
+                client__advisor_id_fk=advisor.id
+            )
+            for client_history in advisor_clients_history:
+                if client_history is not None:
+                    body += (
+                        "Name: "
+                        + client_history.client.names
+                        + " Surname: "
+                        + client_history.client.surnames
+                        + " Email: "
+                        + client_history.client.client_contact_fk.email_address
+                        + "\n"
+                    )
+            if body != "":
+                to.append(advisor.email_address)
+                massMail.append((_subject, body, _from, to))
+                body = ""
+            to = []
 
-        send_mass_mail(datatuple)
+        massMail.append((_subject, _body, _from, _to),)
+
+        send_mass_mail(tuple(massMail), _fail_silently)
+
     except Exception as e:
         log.error(f"Failed to send mail. Error: {e}")
         raise e
