11f6d87a1e3df34f70bb71f38b20b35946435d91
kyle <kylebrandin@gmail.com>
2020-07-06 21:15:29 +0200

    comms.py

--- src/clients/views/communication.py
+++ src/clients/views/communication.py
@@ -1,10 +1,14 @@
+import datetime
+
+from django import forms
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
-from ..models import ClientCommunicationFrequency
+from ..forms import UpdateClientCommunicationHistory
+from ..models import ClientCommunication, ClientCommunicationFrequency
 
 
 class EditClientCommunicationFrequencyView(LoginRequiredMixin, generic.UpdateView):
@@ -33,3 +37,40 @@ class EditClientCommunicationFrequencyView(LoginRequiredMixin, generic.UpdateVie
         )
         self.success_url = reverse_lazy("home")
         return super(EditClientCommunicationFrequencyView, self).form_valid(form)
+
+
+class UpdateClientCommunicationHistoryView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "clients/edit_details.html"
+    model = ClientCommunication
+    form_class = UpdateClientCommunicationHistory
+
+    def form_valid(self, form):
+        home = reverse_lazy("home")
+
+        if "cancel" in self.request.POST:
+            return HttpResponseRedirect(home)
+
+        model = form.save(commit=False)
+
+        today = datetime.date.today()
+        if (
+            model.last_date_email > today
+            or model.last_date_sms > today
+            or model.last_date_call > today
+            or model.last_date_face_to_face > today
+        ):
+            messages.add_message(
+                self.request, messages.ERROR, "Dates cannot be in the future.",
+            )
+            return HttpResponseRedirect(self.request.path_info)
+
+        model.modified_by = self.request.user
+        model.save
+
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            "client communication history successfully updated.",
+        )
+        self.success_url = home
+        return super(UpdateClientCommunicationHistoryView, self).form_valid(form)
