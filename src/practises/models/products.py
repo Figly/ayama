872d5b52ceb12e42e7e44d30b8deaff179f7a576
kyle <kylebrandin@gmail.com>
2020-07-11 11:27:05 +0200

    Ayama-43

---
+++ src/practises/models/products.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+from .base import BaseModel
+
+types = (
+    ("Ins", "Insurance"),
+    ("RA", "Retirement Annuity"),
+    ("UT", "Unit Trust"),
+)
+
+
+class ProductDetail(BaseModel):
+    """
+    Class descriptor
+    """
+
+    product_type = models.CharField(
+        "Product Type", max_length=30, choices=types, default="not specified"
+    )
+    product_name = models.CharField("Product Name", max_length=100)
+    product_company = models.CharField("Product Company", max_length=100)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return f"{self.product_type} {self.product_name}"
