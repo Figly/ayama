51a5e2057de92593834319d7703d6b1a6bfd9337
mareestephan <stephanmaree@gmail.com>
2021-03-24 13:04:58 +0200

    Changes made to design + styling dashboards and summaries after last contact updated

--- src/clients/models/client_contact_detail.py
+++ src/clients/models/client_contact_detail.py
@@ -18,31 +18,31 @@ class ClientContactDetail(BaseModel):
     fax_number = models.CharField("Fax Number", max_length=10, blank=True, null=True)
     email_address = models.EmailField("Email Address", max_length=50)
     residential_address_line_1 = models.CharField(
-        "Residential Address 1", max_length=100
+        "Street Address", max_length=100
     )
     residential_address_line_2 = models.CharField(
-        "Residential Address 2", max_length=100, blank=True, null=True
+        "Apt, Suite, Building or Company Name (Optional)", max_length=100, blank=True, null=True
     )
     residential_suburb = models.CharField(
-        "Residential Suburb", max_length=100, blank=True, null=True
+        "Suburb", max_length=100, blank=True, null=True
     )
     residential_city = models.CharField(
-        "Residential City", max_length=100, blank=True, null=True
+        "City", max_length=100
     )
     residential_country = models.CharField(
-        "Residential Country", max_length=100, blank=True, null=True
+        "Country", max_length=100, blank=True, null=True
     )
-    residential_code = models.IntegerField("Residential Code")
-    postal_address_line_1 = models.CharField("Postal Address 1", max_length=100)
+    residential_code = models.IntegerField("Postal Code")
+    postal_address_line_1 = models.CharField("Street Address", max_length=100)
     postal_address_line_2 = models.CharField(
-        "Postal Addres 2", max_length=100, blank=True, null=True
+        "Apt, Suite, Building or Company Name (Optional)", max_length=100, blank=True, null=True
     )
     postal_suburb = models.CharField(
-        "Postal Suburb", max_length=100, blank=True, null=True
+        "Suburb", max_length=100, blank=True, null=True
     )
     postal_city = models.CharField("Postal City", max_length=100, blank=True, null=True)
     postal_country = models.CharField(
-        "Postal Country", max_length=100, blank=True, null=True
+        "Country", max_length=100, blank=True, null=True
     )
     postal_code = models.IntegerField("Postal Code")
 
